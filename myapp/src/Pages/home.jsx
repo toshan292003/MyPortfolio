0bb61b1095bee88b1f6af2ad7a5a95a96227e98d
toshan292003 <toshanyt@gmail.com>
2024-03-23 17:59:59 +0530

    fdfd

--- myapp/src/Pages/home.jsx
+++ myapp/src/Pages/home.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import './home.css';
 
-export default function Home(){
+export default function Home() {
     return (
         <>
-            <div className="navbar">
-                <p>Hello this is Home page.</p>
+            <div className="home">
+                <section className="home-part1">
+                    <div className="circle">
+                        
+                    </div>
+                    <div>
+                        <h2>Hello I'm <span>TOSHAN S MAINDAN</span></h2>
+                        <h1>UI//UX DESIGNER</h1>
+                    </div>
+                </section>
+                <section className="home-part2">
+
+                </section>
             </div>
         </>
     )
