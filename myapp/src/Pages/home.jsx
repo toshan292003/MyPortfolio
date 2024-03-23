1e717423ec257c826d1af9a5753b57493691ab80
toshan292003 <toshanyt@gmail.com>
2024-03-23 18:23:26 +0530

    fdfdsf

--- myapp/src/Pages/home.jsx
+++ myapp/src/Pages/home.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import './home.css';
+import {useTypewriter,Cursor} from 'react-simple-typewriter';
 
 export default function Home() {
+
+    const [typeEffect] = useTypewriter({
+        words : ['Full Stack Developer','UI/UX Designer','Graphic Designer','Student'],
+        loop:{},
+        typeSpeed : 100,
+        deleteSpeed: 50
+    })
+
     return (
         <>
             <div className="home">
                 <section className="home-part1">
                     <div className="circle">
-                        
+
                     </div>
                     <div>
-                        <h2>Hello I'm <span>TOSHAN S MAINDAN</span></h2>
-                        <h1>UI//UX DESIGNER</h1>
+                        <h2>Hello I'm <b>TOSHAN S MAINDAN</b></h2>
+                        <span>{typeEffect}</span>
+                        <span>
+                            <Cursor cursorStyle='|'/>
+                        </span>
                     </div>
                 </section>
                 <section className="home-part2">
