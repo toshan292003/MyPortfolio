26d6ed39c179a400e44b34569b81a23c3c4480a2
toshan292003 <toshanyt@gmail.com>
2024-03-23 19:45:01 +0530

    dgdg

--- myapp/src/Pages/home.jsx
+++ myapp/src/Pages/home.jsx
@@ -4,6 +4,17 @@ import {useTypewriter,Cursor} from 'react-simple-typewriter';
 
 export default function Home() {
 
+    const RESUME_PDF_FILE = "http://localhost:3000/Resume.pdf";
+    const downloadFile = (url)=>{
+        const fileName = url.split("/").pop();
+        const aTag = document.createElement("a");
+        aTag.href = url;
+        aTag.setAttribute("download",fileName);
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+    };
+
     const [typeEffect] = useTypewriter({
         words : ['Full Stack Developer','UI/UX Designer','Graphic Designer','Student'],
         loop:{},
@@ -19,14 +30,11 @@ export default function Home() {
                     </div>
                     <div>
                         <h2>Hello I'm <b>TOSHAN S MAINDAN</b></h2>
-                        <span>{typeEffect}</span>
-                        <span>
-                            <Cursor cursorStyle='|'/>
-                        </span>
+                        <span>{typeEffect}<Cursor cursorStyle='|'/></span>
+                        <button onClick={() => downloadFile(RESUME_PDF_FILE)}>Download Resume</button>
                     </div>
                 </section>
                 <section className="home-part2">
-
                 </section>
             </div>
         </>
