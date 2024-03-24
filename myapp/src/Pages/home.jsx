9bad40e1f173dc178743dd84f0d2fb698fe8ae83
toshan292003 <toshanyt@gmail.com>
2024-03-24 20:58:03 +0530

    ggsgs

--- myapp/src/Pages/home.jsx
+++ myapp/src/Pages/home.jsx
@@ -1,7 +1,6 @@
 //importing basic react requirements and libraries
-import React from "react";
+import React, { useState ,useEffect } from "react";
 import {useTypewriter,Cursor} from 'react-simple-typewriter';
-
 
 //importing components and pages
 import HomePage from './HomePagePortrait.png';
@@ -9,10 +8,26 @@ import About from './AboutPage.png';
 import AboutBg from '../Images/1x/HomeBackgroundArtboard 1 copy.png';
 import './home.css';
 import CoolHeading from "../components/CoolHeading";
+import homebackimage from '../Images/1x/HomeBackgroundArtboard\ 1.png';
 
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
+    const [OffsetY,setOffsetY] = useState(0);
+
+    const handleScroll = ()=>{
+        setOffsetY(window.pageYOffset);
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll",handleScroll);
+        return ()=> window.removeEventListener("scroll",handleScroll);
+    }, []);
+
+    useEffect(()=>{
+        AOS.init({duration: 1000});
+    });
 
     //function for download option in home page seciton 1
     const RESUME_PDF_FILE = "http://localhost:3000/Resume.pdf";
@@ -37,14 +52,17 @@ export default function Home() {
         <>
             <div className="home">
                 <section className="home-part1">
+                    <div className="homebackimage">
+                        <img src={homebackimage} style={{transform:`translateY(${OffsetY*-0.25}px)`}} alt="" />
+                    </div>
                     <div className="circle">
                     </div>
-                    <div>
+                    <div data-aos="fade-up">
                         <h2>Hello I'm <b>TOSHAN S MAINDAN</b></h2>
                         <span>{typeEffect}<Cursor cursorStyle='|'/></span>
                         <button onClick={() => downloadFile(RESUME_PDF_FILE)}>Download Resume</button>
                     </div>
-                    <img src={HomePage} alt="" />
+                    <img src={HomePage} style={{transform:`translateY(${ OffsetY*0.25}px)`}} alt="" />
                 </section>
 
 
@@ -55,35 +73,35 @@ export default function Home() {
                     <CoolHeading title="about me" subtitle="who am i"></CoolHeading>
                     <p>
                         <section>
-                            <img src={About} alt="" />
+                            <img src={About} style={{transform:`translateY(${-250+ OffsetY*0.25}px)`}} alt="" />
                             <img src={AboutBg} alt="" />
-                            <h1>Something About Me</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa ab reiciendis sit reprehenderit ea soluta illo quae obcaecati quaerat, expedita enim nisi temporibus cum officiis quam error sed eligendi necessitatibus voluptatum ipsam corporis. Aperiam dicta vitae eum. Dolorum nisi inventore ut tempora beatae totam aspernatur, error fugit quisquam soluta quod!</p>
-                            <div>
-                                <ul>
-                                    <li>
+                            <h1 data-aos = "fade-up">Something About Me</h1>
+                            <p data-aos="fade-up">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa ab reiciendis sit reprehenderit ea soluta illo quae obcaecati quaerat, expedita enim nisi temporibus cum officiis quam error sed eligendi necessitatibus voluptatum ipsam corporis. Aperiam dicta vitae eum. Dolorum nisi inventore ut tempora beatae totam aspernatur, error fugit quisquam soluta quod!</p>
+                            <div data-aos="fade-up">
+                                <ul data-aos="fade-up">
+                                    <li data-aos="fade-up">
                                         <h3>Name</h3>
                                         <span>Toshan S Maindan</span>
                                     </li>
-                                    <li>
+                                    <li data-aos="fade-up">
                                         <h3>Email</h3>
                                         <span>toshanyt@gmail.com</span>
                                     </li>
-                                    <li>
+                                    <li data-aos="fade-up">
                                         <h3>Phone</h3>
                                         <span>+91 8549039062</span>
                                     </li>
                                 </ul>
-                                <ul>
-                                    <li>
+                                <ul data-aos="fade-up">
+                                    <li data-aos="fade-up">
                                         <h3>Adress</h3>
                                         <span>Mangalore</span>
                                     </li>
-                                    <li>
+                                    <li data-aos="fade-up">
                                         <h3>DOB</h3>
                                         <span>02 Sept 2003</span>
                                     </li>
-                                    <li>
+                                    <li data-aos="fade-up">
                                         <h3>Occupation</h3>
                                         <span>Student</span>
                                     </li>
@@ -96,6 +114,12 @@ export default function Home() {
 
 
                 <section className="home-part3">
+                    <CoolHeading title="My education" subtitle="Learning journey"></CoolHeading>
+                </section>
+
+
+
+                <section className="home-part4">
                     <CoolHeading title="what i bring to the table" subtitle="my skillset"></CoolHeading>
                 </section>
             </div>
--- myapp/src/components/CoolHeading.jsx
+++ myapp/src/components/CoolHeading.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React,{useEffect} from "react";
 import './coolheading.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function CoolHeading(props) {
+
+    useEffect(()=>{
+        AOS.init({duration: 1000});
+    });
+
     return (
         <>
             <div className="coolheading">
-                <span>{props.subtitle}</span>
-                <h1>{props.title}</h1>
+                <span data-aos = "fade-up">{props.subtitle}</span>
+                <h1 data-aos = "fade-up">{props.title}</h1>
             </div>
         </>
     )
