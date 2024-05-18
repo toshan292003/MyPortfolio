33c12474059edbee74fe071d2f10c163e43b5339
toshan292003 <toshanyt@gmail.com>
2024-05-19 03:25:22 +0530

    fdfs

--- myapp/src/Pages/Experience.jsx
+++ myapp/src/Pages/Experience.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import CoolHeading from "../components/CoolHeading";
+import "./experiences.css";
 
 export default function Experience(){
     return (
         <>
-            <div className="navbar">
-                <p>Hello this is Experience page.</p>
+            <div className="exp">
+                <CoolHeading title="Experiences and Achievements" subtitle="life memories"></CoolHeading>
             </div>
         </>
     )
--- myapp/src/Pages/home.jsx
+++ myapp/src/Pages/home.jsx
@@ -20,6 +20,8 @@ import Dev from "../Images/Icons/coding.png";
 import Motion from "../Images/Icons/lightning.png";
 import Design from "../Images/Icons/webpage.png";
 
+import SmartHome from "../Images/Projects/SmartHome.png";
+
 import "./locomotive.css";
 import LocomotiveScroll from 'locomotive-scroll';
 
@@ -69,22 +71,21 @@ export default function Home() {
     })
 
     const projects=[
-        {name:"Project 1", Image:"https://i.pinimg.com/564x/6e/ca/15/6eca15a37c66d0e2cdc52ae0ef4eecc2.jpg",
+        {name:"JMV Impex", Image:"https://cdn.pixabay.com/photo/2024/04/22/18/53/ai-generated-8713376_1280.jpg",
         Project : {
-            desc:"This is a project sample description",
-            duration : "Sample Duration", 
-            awards : ["This is a sample awards section."],
-            Links : [   "https://github.com/toshan292003","https://leetcode.com/problemset/"],
-            Learnings : ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, sapiente?","Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, sapiente?"]
+            desc:"JMV Impex, a leading mechanical engineering and parts production company, commissioned a full-fledged Landing Page to showcase their capabilities. The visually stunning and user-friendly website, meticulously designed, offers a comprehensive understanding of JMV Impex's expertise, products, and services. With dynamic elements, interactive features, and high-quality visuals, the website brings the company's offerings to life, serving as a powerful marketing tool and solidifying their online presence in the industry.",
+            duration : "3 May - 13 Jun 2023",
+            Links : [   "https://jmvimpex.in/","https://www.figma.com/design/VEAYVe2AMoDEC8l2s4gEuE/JMV-Impex-UI-Design?node-id=8-25&t=GixhHkfeXkFMPLtL-0"],
+            Learnings : ["Effective team and client communication strategies.","Understanding the basic workflow of website development projects."]
         }
         },
-        {name:"Project 2", Image:"https://i.pinimg.com/564x/25/56/21/2556210e95b6e5d8debf48ce58678b07.jpg",
+        {name:"Smart House", Image:SmartHome,
         Project : {
-            desc:"This is a project sample description 2",
-            duration : "Sample Duration 2",
-            awards : ["Award 1"],
-            Links : ["https://github.com/toshan292003","https://leetcode.com/problemset/"],
-            Learnings : ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, sapiente?","Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, sapiente?"]
+            desc:"This award-winning UI/UX design revolutionizes home automation through an intuitive smartphone app. Users can effortlessly monitor and control all home appliances, turning them on/off remotely. The real-time status updates allow seamless tracking of each device's condition. This innovative solution streamlines home management, making it convenient and user-friendly.",
+            duration : "29 Apr - 29 Apr 2023",
+            awards : ["Secured First Place in UI/UX Competition conducted by YentechMania 2024 at Yenepoya IT, Moodibidri"],
+            Links : ["https://www.figma.com/design/7g2D7hnOL459uUy86Y0yW5/Untitled?t=tlhoQg5rRcl07l2G-0"],
+            Learnings : ["The competition honed my problem-solving skills, enabling me to approach challenges with a fresh perspective."]
             }
         },
         {name:"Project 3", Image:"https://i.pinimg.com/564x/ad/c1/8f/adc18f7eb9df78a4ffa2f2441ae69b2d.jpg",
