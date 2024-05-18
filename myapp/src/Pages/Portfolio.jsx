9820525cd75869bf774e92b716560e3d5b4bc653
toshan292003 <toshanyt@gmail.com>
2024-05-19 01:47:03 +0530

    fsgg

--- myapp/src/Pages/Portfolio.jsx
+++ myapp/src/Pages/Portfolio.jsx
@@ -9,8 +9,8 @@ export default function Portfolio(){
         <>
         <div className="portfolio">
             <Part title="Web Development" image=""></Part>
-            <Part title="Designing" image=""></Part>
-            <Part title="Cinematography" image=""></Part>
+            <Part title="Designing" image="https://cdn.pixabay.com/photo/2024/04/06/05/30/ai-generated-8678698_1280.jpg"></Part>
+            <Part title="Cinematography" image="https://images.pexels.com/photos/821749/pexels-photo-821749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></Part>
         </div>
         </>
     )
